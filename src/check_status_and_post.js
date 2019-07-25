61ff3f6fdaf4d1e78c9ded11c98d4ce2e80bedc5
Yoko Li <yoko@transposit.com>
2019-07-25 02:03:04 +0000

    Commit from Transposit IDE

--- src/check_status_and_post.js
+++ src/check_status_and_post.js
@@ -7,27 +7,27 @@
     // try to ping athena
     try {
        await new Promise((resolve) => {
-        setTimeout(() => {
+        setTimeout(() => {         
+          result = api
+            .run("aws_athena.get_query_results", { QueryExecutionId: executionId })
+            .map(e => {
+              return e.Data;
+            });
+          const cols = result[0];
+          result = result.slice(1, result.length);
+          let formattedMsg = result.map(e => {
+            return cols.reduce((obj, k, i) => ({ ...obj, [k]: e[i] }), {});
+          });
+          stash.put(params.response_url, null);
+        console.log(formattedMsg);
+          return api.run("slack_webhook.post_to_response_url", {
+            response_url: params.response_url,
+            post_body: { text: JSON.stringify(formattedMsg) }
+          });
           resolve();
         }, 5000)
       });
 
-      result = api
-        .run("aws_athena.get_query_results", { QueryExecutionId: executionId })
-        .map(e => {
-          return e.Data;
-        });
-      const cols = result[0];
-      result = result.slice(1, result.length);
-      let formattedMsg = result.map(e => {
-        return cols.reduce((obj, k, i) => ({ ...obj, [k]: e[i] }), {});
-      });
-      stash.put(params.response_url, null);
-	console.log(formattedMsg);
-      return api.run("slack_webhook.post_to_response_url", {
-        response_url: params.response_url,
-        post_body: { text: JSON.stringify(formattedMsg) }
-      });
     } catch (e) {
       console.log(e);
     }
