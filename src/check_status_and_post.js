afcc9fc3a9a6337d99f2db752c674347fb376829
Yoko Li <yoko@transposit.com>
2019-07-25 00:47:31 +0000

    Commit from Transposit IDE

--- src/check_status_and_post.js
+++ src/check_status_and_post.js
@@ -1,31 +1,37 @@
-(params) => {
+async params => {
   console.log(stash.get(params.response_url));
-	while(stash.get(params.response_url) != null) {
-      let executionId = stash.get(params.response_url);
-      let result;
-      // try to ping athena 
-      try {
+  while (stash.get(params.response_url) != null) {
+    let executionId = stash.get(params.response_url);
+    let result;
+    // try to ping athena
+    try {
+      await new Promise(resolve => {
         setTimeout(() => {
-          result = api.run("aws_athena.get_query_results", { QueryExecutionId: executionId }).map(e => {
-        		return e.Data;
-      		});
-          const cols = result[0];
-  		  result = result.slice(1, result.length);
-          let formattedMsg = result.map(e => {
-              return cols.reduce((obj, k, i) => ({ ...obj, [k]: e[i] }), {});
-          });
-          stash.put(params.response_url, null);
-          
-          return api.run("slack_webhook.post_to_response_url", {
-            response_url: params.response_url,
-            post_body: { text:  JSON.stringify(formattedMsg) }
-          });
+          resolve();
         }, 5000)
-      } catch (e) {
-        console.log(e);
-      }
+      });
+
+      result = api
+        .run("aws_athena.get_query_results", { QueryExecutionId: executionId })
+        .map(e => {
+          return e.Data;
+        });
+      const cols = result[0];
+      result = result.slice(1, result.length);
+      let formattedMsg = result.map(e => {
+        return cols.reduce((obj, k, i) => ({ ...obj, [k]: e[i] }), {});
+      });
+      stash.put(params.response_url, null);
+
+      return api.run("slack_webhook.post_to_response_url", {
+        response_url: params.response_url,
+        post_body: { text: JSON.stringify(formattedMsg) }
+      });
+    } catch (e) {
+      console.log(e);
     }
-}
+  }
+};
 
 /*
  * For sample code and reference material, visit
