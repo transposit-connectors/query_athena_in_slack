9281af2054dfa7b6ae55bae91376de3831d1cf99
Yoko Li <yoko@transposit.com>
2019-07-25 23:25:16 +0000

    Commit from Transposit IDE

--- src/untitled_1.js
+++ src/untitled_1.js
@@ -1,11 +1,22 @@
 (params) => {
  const moment = require('moment.js');
+  const _ = require('underscore.js');
 const yesterday = moment().subtract("1", "day");
  const lookupTime = yesterday.format('YYYY-MM-DD') + " 00:00:00";
-  let data = api.run('this.get_sheet_values')[0].values;
+  let data = api.run('this.get_signups_sheet_values')[0].values;
+  const cols = data[0];
+  data = data.slice(1, data.length);
+  formattedData = data.map(e => {
+    	return cols.reduce((obj, k, i) => ({ ...obj, [k]: e[i] }), {});
+  	});
   
-  return data
- 
+  let text = "";
+  formattedData.forEach((d) => {
+    if (d.Dte === lookupTime) {
+      text = text + "on " + yesterday.format('YYYY-MM-DD') + ", Number of Signup = " + d.Count + ", rolling sum (28 days) = " + d["sum of past 28 days"];
+    }
+  })
+  return text
 }
 
 /*
