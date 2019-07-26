9c3a8457652f55d7b5c3635968e88c1375f6eb78
Yoko Li <yoko@transposit.com>
2019-07-26 00:49:47 +0000

    Commit from Transposit IDE

--- src/untitled_1.js
+++ src/untitled_1.js
@@ -16,7 +16,7 @@ params => {
     sheetId: env.get("activeUserSheetId")
   })[0].values);
 
-  return secondAppData
+  return activeUsersData;
   
   const cols = data[0];
   signupData = data.slice(1, data.length);
