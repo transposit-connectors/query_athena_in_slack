326910ba0c3b9b0c6df689c40a9a6af3854281c2
Yoko Li <yoko@transposit.com>
2019-08-20 22:16:36 +0000

    Commit from Transposit IDE

--- src/check_status.js
+++ src/check_status.js
@@ -13,14 +13,19 @@
       let formattedMsg = result.map(e => {
         return cols.reduce((obj, k, i) => ({ ...obj, [k]: e[i] }), {});
       });
-
-     return api.run("slack_webhook.post_to_response_url", {
-        response_url: params.stashId,
-        post_body: { text: JSON.stringify(formattedMsg) }
-      });
+		
+      // delete task if data came back
+      let key = params.stashId + "task";
+      let taskUUID = stash.get(key);
+      task.delete(taskUUID);
     } catch (error) {
       return "Run has not finished/Run contains bad query";
     }
+        
+   return api.run("slack_webhook.post_to_response_url", {
+      response_url: params.stashId,
+      post_body: { text: JSON.stringify(formattedMsg) }
+    });
   }
 
   throw new Error(`stashId ${params.stashId} does not exist!`);
