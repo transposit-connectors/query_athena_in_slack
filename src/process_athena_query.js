87cc806c528d021bad3d36e20e7c17dc78db2f93
Yoko Li <yoko@transposit.com>
2019-08-20 21:42:51 +0000

    Commit from Transposit IDE

--- src/process_athena_query.js
+++ src/process_athena_query.js
@@ -1,7 +1,6 @@
 (params) => {
-  
-  let taskId = task.create("test").runEvery(5, "SECONDS");
-
+  let taskId = task.create("this.check_status", {stashId: params.stashId}).runEvery(5, "SECONDS");
+	
 	
   //stash.put("https://hooks.slack.com/commands/T2615V5UK/698942962977/bFBw3v3S9GaUyJPiUssy9yDD", "31e220f5-2e8b-4bff-8276-26073b9a2eca")
 }
