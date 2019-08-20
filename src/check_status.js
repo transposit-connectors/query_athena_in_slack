3cee4e7ba66a2c50114d3d999d0635a35f4fc9ae
Yoko Li <yoko@transposit.com>
2019-08-20 22:10:16 +0000

    Commit from Transposit IDE

--- src/check_status.js
+++ src/check_status.js
@@ -1,11 +1,22 @@
 (params) => {
- 
   let executionId = stash.get(params.stashId);
   if (executionId) {
-    return api.run("aws_athena.get_query_results", { QueryExecutionId: executionId });
+    try {
+      let result = api
+        .run("aws_athena.get_query_results", { QueryExecutionId: executionId })
+        .map(e => {
+          return e.Data;
+        });
+      api.run("slack_webhook.post_to_response_url", {
+        response_url: params.stashId,
+        post_body: { text: JSON.stringify(formattedMsg) }
+      });
+    } catch (error) {
+      return "Run has not finished/Run contains bad query";
+    }
   }
-  
-  throw new Error(`stashId ${params.stashId} does not exist!`)
+
+  throw new Error(`stashId ${params.stashId} does not exist!`);
 }
 /*
  * For sample code and reference material, visit
--- src/process_athena_query.js
+++ src/process_athena_query.js
@@ -1,7 +1,7 @@
 (params) => {
-  let taskId = task.create("this.check_status", {stashId: params.stashId}).runOnce("2019-08-20T21:46:59.537Z");
+  let taskUUID = task.create("this.check_status", {stashId: params.stashId}).runEvery(5, "SECONDS");
 	
-	
+	//"2019-08-20T21:46:59.537Z"
   //stash.put("https://hooks.slack.com/commands/T2615V5UK/698942962977/bFBw3v3S9GaUyJPiUssy9yDD", "31e220f5-2e8b-4bff-8276-26073b9a2eca")
 }
 
