c62a0aa02dddedae83bdd2849042328984d72fcd
Yoko Li <yoko@transposit.com>
2019-08-21 20:35:28 +0000

    Commit from Transposit IDE

--- src/process_athena_query.js
+++ src/process_athena_query.js
@@ -3,6 +3,7 @@
   let taskUUID = task.create("this.check_status", {stashId: params.stashId}).runEvery(5, "SECONDS");
   console.log(params.stashId+"task");
   stash.put("TASK-"+params.stashId, taskUUID);
+  stash.put("COUNTER-"+params.stashId, 0);
 }
 
 /*
