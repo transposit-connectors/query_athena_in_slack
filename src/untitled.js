b4c855347640f4945b9ec323416e97a6d693e451
Yoko Li <yoko@transposit.com>
2019-08-21 20:25:33 +0000

    Updated tasks to not listTask

--- src/untitled.js
+++
@@ -1,9 +0,0 @@
-(params) =>{
-  r
-  return task.delete('b3cb66c6-100e-43bd-917d-50b5cc28913c')
-}
-
-/*
- * For sample code and reference material, visit
- * https://www.transposit.com/docs/references/js-operations
- */
