8b69a5bf331c118ae2ab5c52fa6a1b275dbef588
Yoko Li <yoko@transposit.com>
2019-08-27 00:38:44 +0000

    Commit from Transposit IDE

--- src/query_athena.js
+++ src/query_athena.js
@@ -1,47 +1,14 @@
- (params) => {
-  
+(params) => {
   let executionId = api.run("aws_athena.start_query_execution", {
-      QueryString : params.query,
-      ClientRequestToken: makeid(35),
-      WorkGroup: "primary",
-      ResultConfiguration: {OutputLocation: 's3://prod.events-logs.transposit.com/query-results'}
-    })[0]['QueryExecutionId'];
+    QueryString: params.query,
+    ClientRequestToken: makeid(35),
+    WorkGroup: "primary",
+    ResultConfiguration: {
+      OutputLocation: "s3://prod.events-logs.transposit.com/query-results"
+    }
+  })[0]["QueryExecutionId"];
 
   return executionId;
-  let result;
-  result = result.slice(1, result.length);
-  let formattedData = result.map(e => {
-      return cols.reduce((obj, k, i) => ({ ...obj, [k]: e[i] }), {});
-  });
-  let message = JSON.stringify(formattedData);
-  console.log(message);
-  return api.run("slack_webhook.post_to_response_url", {
-        response_url: params.response_url,
-        post_body: {
-          // The blocks get displayed in the actual message. 
-          // You can play with block kit here: https://api.slack.com/tools/block-kit-builder
-          blocks: [{
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": message
-            }
-          }],
-          // The text content gets displayed in the notification
-          text: 'A message from Transposit!'
-        }
-      }); 
-  
-  
-  function makeid(length) {
-    let text = "";
-    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for (let i = 0; i < length; i++)
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-    return text;
-  }
 }
 
 /*
