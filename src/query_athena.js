e796acaa368af305004101d12b6d4735e31fc2ee
Yoko Li <yoko@transposit.com>
2019-08-27 00:52:04 +0000

    Commit from Transposit IDE

--- src/query_athena.js
+++ src/query_athena.js
@@ -9,6 +9,16 @@
   })[0]["QueryExecutionId"];
 
   return executionId;
+  
+  function makeid(length) {
+    let text = "";
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < length; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+  }
 }
 
 /*
